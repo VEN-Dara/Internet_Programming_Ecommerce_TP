9433a6f83d66cc7d55c900b5b22044e493783653
VEN-Dara <vendara.389@gmail.com>
2024-07-05 17:24:10 +0700

    TP08

--- Ecommerce/vite.config.js
+++ Ecommerce/vite.config.js
@@ -1,19 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Internet_Programming_Ecommerce_TP/',
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
